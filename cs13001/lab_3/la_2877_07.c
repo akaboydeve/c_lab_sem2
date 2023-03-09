152cd701b087ac307702c695fb6338a1728eaf5b
akaboydeve <akashsardaryt@gmail.com>
2023-03-09 19:36:40 +0530

    Update la_2877_07.c

--- cs13001/lab_3/la_2877_07.c
+++ cs13001/lab_3/la_2877_07.c
@@ -1,10 +1,18 @@
-#include<stdio.h>
+// range of unsigned int data type
+#include <stdio.h>
 
 int main()
 {
+    unsigned int max = 0;
+    unsigned int i = 0;
 
-
-
-
-return 0;
+    for (i = 1; i > max; i++)
+    {
+        if (i != 0)
+        {
+            max = i;
+        }
+    }
+    printf("The maximum value of unsigned int is: %d\n", max);
+    return 0;
 }
