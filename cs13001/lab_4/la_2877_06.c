9d2cfeb09c0fe134761c1a4d8c55b61c44be2a0f
root <root@AKASH>
2023-03-21 13:35:27 +0530

    lab4 array and pointer

--- cs13001/lab_4/la_2877_06.c
+++ cs13001/lab_4/la_2877_06.c
@@ -2,13 +2,36 @@
 
 int main ()
 {
+int Arr[5];
 
-int stu[5], sub[5];
-int i , j ;
+int i ,j , max , min ;
 
-printf("");
+for(i = 0 ; i<5 ;i++)
+{
+printf("Enter Element No %d:",i+1);
+scanf("%d",&Arr[i]);
+}
 
+printf("The Array is \n");
+for(i = 0 ; i<5 ;i++)
+{
+printf("%d\t",Arr[i]);
+}
+
+for ( i = 1; i < 5; ++i)
+{
+if (Arr[0] < Arr[i])
+{
+Arr[0] = Arr[i];
+}
+}
+printf("\nMax Element is %d\n",Arr[0]);
+
+
+for ( i = 1; i < 5; ++i)
+{
 
 
 return 0;
 }
+
